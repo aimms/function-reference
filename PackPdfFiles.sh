eb0a528297421a07fc148b649fbb586cb6fcbb49
Anna <anna.zenkova@aimms.com>
2020-09-07 10:55:14 +0200

    engine: pack pdf file to a yabr package if master branch or  [yabr] specified

---
+++ PackPdfFiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# call with yabr verb : commit or push
+# creating the doxygen files and pushing it to the yabr repository
+
+#set current directory here
+cd "${0%/*}"
+
+mkdir -p "target/all"
+cp _build/latex/AIMMS_func.pdf target/all
+../../buildtool/yabr.sh -release -yabr yabr.xml -toolset all $1
+
+python -u writeVersionAndBranchToDB.py
+
+
