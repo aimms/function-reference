28632ecfb13b94aa4368adf4d9d53a7f059ee1d7
Arthur dHerbemont <arthur.dherbemont@aimms.com>
2021-01-12 16:06:46 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -125,6 +125,7 @@ html_theme_options = {
     'generate_google_analytics' : Display_3rd_Party_Extensions,
 }
 
+html_last_updated_fmt = '%b %d, %Y'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
